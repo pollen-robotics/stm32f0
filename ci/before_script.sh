c0fd3a02f34b7b5eee51a7292734263cc327491b
Pierre Rouanet <pierre.rouanet@gmail.com>
2017-10-11 13:29:15 +0200

    Make sure we build xargo outside of the repo to compile it in x86.

--- ci/before_script.sh
+++ ci/before_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if [ $TARGET = x86_64-unknown-linux-gnu ]; then
-  which xargo || cargo install xargo -f --vers 0.3.8
+  which xargo || (cd .. && cargo install xargo -f --vers 0.3.8)
 fi
