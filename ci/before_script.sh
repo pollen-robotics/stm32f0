3f8989069db1b9b9b28f653aef5fd04ebef4a826
Pierre Rouanet <pierre.rouanet@gmail.com>
2017-10-11 13:32:54 +0200

    Also install rust-src via rustup.

--- ci/before_script.sh
+++ ci/before_script.sh
@@ -2,4 +2,6 @@
 
 if [ $TARGET = x86_64-unknown-linux-gnu ]; then
   which xargo || (cd .. && cargo install xargo -f --vers 0.3.8)
+  rustup component list | grep 'rust-src.*installed' || \
+    rustup component add rust-src
 fi
