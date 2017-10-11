f696198267e35196c7cf14d6231fd012ad713421
Pierre Rouanet <pierre.rouanet@gmail.com>
2017-10-11 14:58:20 +0200

    Add skeleton for the stm32f0_hal crate.

---
+++ stm32f0_hal/examples/blinky.rs
@@ -0,0 +1,20 @@
+#![feature(used)]
+#![no_std]
+
+extern crate cortex_m_rt;
+extern crate cortex_m;
+use cortex_m::asm;
+
+extern crate stm32f0_hal;
+
+fn main() {
+}
+
+// As we are not using interrupts, we just register a dummy catch all handler
+#[link_section = ".vector_table.interrupts"]
+#[used]
+static INTERRUPTS: [extern "C" fn(); 240] = [default_handler; 240];
+
+extern "C" fn default_handler() {
+    asm::bkpt();
+}
