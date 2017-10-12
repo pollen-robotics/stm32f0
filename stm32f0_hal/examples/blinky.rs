3dc99a8da34e88a28992b8384023bcb5e3853eb6
Pierre Rouanet <pierre.rouanet@gmail.com>
2017-10-12 17:37:34 +0200

    Add example.

--- stm32f0_hal/examples/blinky.rs
+++ stm32f0_hal/examples/blinky.rs
@@ -6,8 +6,19 @@ extern crate cortex_m;
 use cortex_m::asm;
 
 extern crate stm32f0_hal;
+use stm32f0_hal::gpio;
 
-fn main() {}
+const BTN_PIN: gpio::Pin = gpio::Pin::P6;
+const LED_PIN: gpio::Pin = gpio::Pin::P9;
+
+fn main() {
+    gpio::init(&BTN_PIN, &gpio::Mode::Input);
+    gpio::init(&LED_PIN, &gpio::Mode::Output);
+
+    loop {
+        gpio::write(&LED_PIN, gpio::read(&BTN_PIN));
+    }
+}
 
 // As we are not using interrupts, we just register a dummy catch all handler
 #[link_section = ".vector_table.interrupts"]
