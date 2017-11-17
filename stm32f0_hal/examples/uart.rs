69d0e5197dd7ed29746d31b85dc11470c06fbb4b
Cyril SAGONÉRO <csagonero@free.fr>
2017-11-17 17:35:27 +0100

    Add uart example
    Signed-off-by: Cyril SAGONÉRO <csagonero@free.fr>

---
+++ stm32f0_hal/examples/uart.rs
@@ -0,0 +1,17 @@
+#![feature(used)]
+#![no_std]
+extern crate stm32f0_hal as hal;
+extern crate cortex_m;
+
+use hal::rcc;
+use hal::uart;
+
+fn main() {
+    rcc::init(); // Full Speed 48Mhz
+    uart::init(6000000,uart::NBITS::_8bits,uart::STOPBITS::_1b,uart::PARITY::None);
+    loop {
+        if uart::transmit_complete() {
+            uart::send(0x55);
+        }
+    }
+}
