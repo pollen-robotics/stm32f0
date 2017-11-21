8c61fafcea4490aedb1ce826e07f0b1b08187e8a
Cyril SAGONÉRO <csagonero@free.fr>
2017-11-21 12:49:30 +0100

    Fix : correct number of interrupts
    Signed-off-by: Cyril SAGONÉRO <csagonero@free.fr>

--- stm32f0_hal/examples/pwm.rs
+++ stm32f0_hal/examples/pwm.rs
@@ -22,7 +22,7 @@ fn main() {
 // As we are not using interrupts, we just register a dummy catch all handler
 #[link_section = ".vector_table.interrupts"]
 #[used]
-static INTERRUPTS: [extern "C" fn(); 240] = [default_handler; 240];
+static INTERRUPTS: [extern "C" fn(); 128] = [default_handler; 128];
 
 extern "C" fn default_handler() {
     asm::bkpt();
