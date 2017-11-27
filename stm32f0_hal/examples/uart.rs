f6823d5715c6bbf645432fad89413b9a7d8ab137
Cyril SAGONÉRO <csagonero@free.fr>
2017-11-27 17:02:19 +0100

    Update uart example
    Signed-off-by: Cyril SAGONÉRO <csagonero@free.fr>

--- stm32f0_hal/examples/uart.rs
+++ stm32f0_hal/examples/uart.rs
@@ -8,15 +8,17 @@ use hal::uart;
 
 fn main() {
     rcc::init(); // Full Speed 48Mhz
-    uart::init(
+    uart::setup(
         6000000,
         uart::NBits::_8bits,
         uart::StopBits::_1b,
         uart::Parity::None,
+        |byte| uart::send(byte),
     );
+    if uart::transmit_complete() {
+        uart::send(0x55);
+    }
     loop {
-        if uart::transmit_complete() {
-            uart::send(0x55);
-        }
+
     }
 }
