3dd901464e1edddd312d42bd8f86d73a4cc57de2
Cyril SAGONÉRO <csagonero@free.fr>
2018-01-25 18:23:33 +0100

    New PWM implementation Support PC6/PC7/PC8/PC9 and PB4/PB5 TIMER3
    Signed-off-by: Cyril SAGONÉRO <csagonero@free.fr>

--- stm32f0_hal/examples/pwm.rs
+++ stm32f0_hal/examples/pwm.rs
@@ -3,11 +3,22 @@
 extern crate stm32f0_hal as hal;
 
 use hal::pwm;
+use hal::rcc;
 
 fn main() {
-    pwm::init(10000);
-    pwm::set_duty(750);
-    pwm::enable();
-
+    rcc::init();
+    let channel1 = pwm::Pwm::init(pwm::Pin::PC6);
+    let channel2 = pwm::Pwm::init(pwm::Pin::PC7);
+    let channel3 = pwm::Pwm::init(pwm::Pin::PC8);
+    let channel4 = pwm::Pwm::init(pwm::Pin::PC9);
+    channel1.set_frequency(49);
+    channel1.set_duty(10);
+    channel2.set_duty(20);
+    channel3.set_duty(30);
+    channel4.set_duty(40);
+    channel1.enable();
+    channel2.enable();
+    channel3.enable();
+    channel4.enable();
     loop {}
 }
