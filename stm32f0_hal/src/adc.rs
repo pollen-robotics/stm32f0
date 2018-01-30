5b3b4aa08dfaded5cd8c7dd1f598724a19464d8d
Jonathan Grizou <jonathan.grizou@gmail.com>
2018-01-30 10:47:50 +0100

    renaming channel to pin renaming ADC to PA or PC

--- stm32f0_hal/src/adc.rs
+++ stm32f0_hal/src/adc.rs
@@ -20,21 +20,21 @@
 use cortex_m;
 use stm32f0x2::{ADC, GPIOA, GPIOC, RCC};
 
-/// ADC Channel
-pub enum Channel {
-    ADC0,
-    ADC1,
-    ADC13,
-    ADC14,
+/// ADC Pin
+pub enum Pin {
+    PA0,
+    PA1,
+    PC13,
+    PC14,
 }
 
 /// Input Mode Pin
 pub struct Analog {
-    pin: Channel,
+    pin: Pin,
 }
 impl Analog {
     /// Setup a PIN in Input Mode
-    pub fn setup(pin: Channel) -> Analog {
+    pub fn setup(pin: Pin) -> Analog {
         setup_pin(&pin);
         Analog { pin }
     }
@@ -43,12 +43,12 @@ impl Analog {
         cortex_m::interrupt::free(|cs| {
             let adc = ADC.borrow(cs);
 
-            // ADC Channel selection
+            // ADC Pin selection
             match self.pin {
-                Channel::ADC0 => adc.chselr.write(|w| w.chsel0().set_bit()),
-                Channel::ADC1 => adc.chselr.write(|w| w.chsel1().set_bit()),
-                Channel::ADC13 => adc.chselr.write(|w| w.chsel13().set_bit()),
-                Channel::ADC14 => adc.chselr.write(|w| w.chsel14().set_bit()),
+                Pin::PA0 => adc.chselr.write(|w| w.chsel0().set_bit()),
+                Pin::PA1 => adc.chselr.write(|w| w.chsel1().set_bit()),
+                Pin::PC13 => adc.chselr.write(|w| w.chsel13().set_bit()),
+                Pin::PC14 => adc.chselr.write(|w| w.chsel14().set_bit()),
             }
 
             // Active ADC and Start Conversion
@@ -63,28 +63,28 @@ impl Analog {
     }
 }
 
-fn setup_pin(pin: &Channel) {
+fn setup_pin(pin: &Pin) {
     cortex_m::interrupt::free(|cs| {
         let rcc = RCC.borrow(cs);
-        let gpioc = GPIOC.borrow(cs);
         let gpioa = GPIOA.borrow(cs);
+        let gpioc = GPIOC.borrow(cs);
         // Clock Activation
         match *pin {
-            Channel::ADC0 => rcc.ahbenr.modify(|_, w| w.iopaen().enabled()),
-            Channel::ADC1 => rcc.ahbenr.modify(|_, w| w.iopaen().enabled()),
-            Channel::ADC13 => rcc.ahbenr.modify(|_, w| w.iopcen().enabled()),
-            Channel::ADC14 => rcc.ahbenr.modify(|_, w| w.iopcen().enabled()),
+            Pin::PA0 => rcc.ahbenr.modify(|_, w| w.iopaen().enabled()),
+            Pin::PA1 => rcc.ahbenr.modify(|_, w| w.iopaen().enabled()),
+            Pin::PC13 => rcc.ahbenr.modify(|_, w| w.iopcen().enabled()),
+            Pin::PC14 => rcc.ahbenr.modify(|_, w| w.iopcen().enabled()),
         }
 
         // Clock activation ADC
         rcc.apb2enr.modify(|_, w| w.adcen().enabled());
 
-        // PA Analog Input Channel
+        // PA Analog Input Pin
         match *pin {
-            Channel::ADC0 => gpioa.moder.modify(|_, w| w.moder0().analog()),
-            Channel::ADC1 => gpioa.moder.modify(|_, w| w.moder1().analog()),
-            Channel::ADC13 => gpioc.moder.modify(|_, w| w.moder3().analog()),
-            Channel::ADC14 => gpioc.moder.modify(|_, w| w.moder4().analog()),
+            Pin::PA0 => gpioa.moder.modify(|_, w| w.moder0().analog()),
+            Pin::PA1 => gpioa.moder.modify(|_, w| w.moder1().analog()),
+            Pin::PC13 => gpioc.moder.modify(|_, w| w.moder3().analog()),
+            Pin::PC14 => gpioc.moder.modify(|_, w| w.moder4().analog()),
         }
     });
 }
