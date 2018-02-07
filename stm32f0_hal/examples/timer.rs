7a46c7e9db4d07e5425fafa3ec7790b4fe2edb85
Pierre Rouanet <pierre.rouanet@gmail.com>
2018-02-07 15:48:41 +0100

    Finish timer sync implementation.

--- stm32f0_hal/examples/timer.rs
+++ stm32f0_hal/examples/timer.rs
@@ -7,13 +7,40 @@ use hal::stm32f0x2;
 use hal::timer::Timer;
 use hal::prelude::*;
 
+extern crate embedded_hal;
+use embedded_hal::prelude::*;
+
+#[macro_use(block)]
+extern crate nb;
+
 fn main() {
     let p = stm32f0x2::Peripherals::take().unwrap();
+
+    let mut flash = p.FLASH.constrain();
+
     let mut rcc = p.RCC.constrain();
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
-    let timer = Timer::tim7(p.TIM7, 1000_u32.hz(), &mut rcc.apb1);
-    // allumer une led quand on appuie sur un bouton
-    // + timer qui va l'eteindre au bout de 1000 ms
+    let mut timer = Timer::tim6(p.TIM6, clocks, &mut rcc.apb1);
 
-    loop {}
+    let mut gpioc = p.GPIOC.split(&mut rcc.ahb);
+    let mut led = gpioc
+        .pc7
+        .into_push_pull_output(&mut gpioc.moder, &mut gpioc.otyper);
+
+    let mut gpioa = p.GPIOA.split(&mut rcc.ahb);
+    let button = gpioa
+        .pa0
+        .into_floating_input(&mut gpioa.moder, &mut gpioa.pupdr);
+
+    loop {
+        if button.is_high() {
+            led.set_high();
+
+            timer.start(1_u32.hz());
+            block!(timer.wait()).ok();
+
+            led.set_low();
+        }
+    }
 }
--- stm32f0_hal/src/timer.rs
+++ stm32f0_hal/src/timer.rs
@@ -1,33 +1,27 @@
 use hal::timer::CountDown;
-use nb::Result;
+use nb::{Error, Result};
 
 use time::Hertz;
-use stm32f0x2::TIM7;
+use stm32f0x2::{TIM6, TIM7};
 
-use rcc::APB1;
+use rcc::{APB1, Clocks};
 
 pub struct Timer<TIM> {
     tim: TIM,
+    clocks: Clocks,
 }
 
 macro_rules! timer {
     ($($TIMX:ident: ($timx:ident, $APBX:ident, $timxen:ident, $timxrst:ident), )+) => {
         $(
         impl Timer<$TIMX> {
-            pub fn $timx<T>(tim: $TIMX, timeout: T, apb: &mut $APBX) -> Self
-            where T: Into<Hertz> {
+            pub fn $timx(tim: $TIMX, clocks: Clocks, apb: &mut $APBX) -> Self
+            {
                 apb.enr().modify(|_, w| w.$timxen().enabled());
                 apb.rstr().modify(|_, w| w.$timxrst().set_bit());
                 apb.rstr().modify(|_, w| w.$timxrst().clear_bit());
 
-                let mut timer = Timer { tim };
-                timer.configure(timeout);
-                timer
-            }
-            fn configure<T>(&mut self, _timeout: T)
-            where T: Into<Hertz> {
-                self.tim.cr1.modify(|_, w| w.cen().disabled());
-                // TODO: WIP
+                Timer { clocks, tim }
             }
         }
         impl CountDown for Timer<$TIMX> {
@@ -35,16 +29,33 @@ macro_rules! timer {
             fn start<T>(&mut self, timeout: T)
             where
                 T: Into<Hertz> {
-                self.configure(timeout);
-                // TODO: WIP
+                self.tim.cr1.modify(|_, w| w.cen().disabled());
+                self.tim.cnt.reset();
+
+                let frequency = timeout.into().0;
+                let ticks = self.clocks.pclk().0 / frequency;
+
+                let psc = (ticks - 1) / (1 << 16);
+                self.tim.psc.write(|w| w.psc().bits(psc as u16));
+
+                let arr = ticks / (psc + 1);
+                self.tim.arr.write(|w| w.arr().bits(arr as u16));
+
+                self.tim.cr1.modify(|_, w| w.cen().enabled());
             }
             fn wait(&mut self) -> Result<(), !> {
-                // TODO: WIP
-                Ok(())
+                if self.tim.sr.read().uif().bit_is_clear() {
+                    Err(Error::WouldBlock)
+                } else {
+                    // Clean event flat
+                    self.tim.sr.modify(|_, w| w.uif().clear());
+                    Ok(())
+                }
             }
         }
         )+
     }
 }
 
+timer!(TIM6: (tim6, APB1, tim6en, tim6rst),);
 timer!(TIM7: (tim7, APB1, tim7en, tim7rst),);
